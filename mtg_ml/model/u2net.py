852fafa703d682ece94f9ec6c974725c9e910e5f
Nathan Michlo <NathanJMichlo@gmail.com>
2021-12-06 12:25:33 +0200

    add u2net

---
+++ mtg_ml/model/u2net.py
@@ -0,0 +1,399 @@
+#                               Apache License
+#                         Version 2.0, January 2004
+#                      http://www.apache.org/licenses/
+#
+# TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+#
+# 1. Definitions.
+#
+#    "License" shall mean the terms and conditions for use, reproduction,
+#    and distribution as defined by Sections 1 through 9 of this document.
+#
+#    "Licensor" shall mean the copyright owner or entity authorized by
+#    the copyright owner that is granting the License.
+#
+#    "Legal Entity" shall mean the union of the acting entity and all
+#    other entities that control, are controlled by, or are under common
+#    control with that entity. For the purposes of this definition,
+#    "control" means (i) the power, direct or indirect, to cause the
+#    direction or management of such entity, whether by contract or
+#    otherwise, or (ii) ownership of fifty percent (50%) or more of the
+#    outstanding shares, or (iii) beneficial ownership of such entity.
+#
+#    "You" (or "Your") shall mean an individual or Legal Entity
+#    exercising permissions granted by this License.
+#
+#    "Source" form shall mean the preferred form for making modifications,
+#    including but not limited to software source code, documentation
+#    source, and configuration files.
+#
+#    "Object" form shall mean any form resulting from mechanical
+#    transformation or translation of a Source form, including but
+#    not limited to compiled object code, generated documentation,
+#    and conversions to other media types.
+#
+#    "Work" shall mean the work of authorship, whether in Source or
+#    Object form, made available under the License, as indicated by a
+#    copyright notice that is included in or attached to the work
+#    (an example is provided in the Appendix below).
+#
+#    "Derivative Works" shall mean any work, whether in Source or Object
+#    form, that is based on (or derived from) the Work and for which the
+#    editorial revisions, annotations, elaborations, or other modifications
+#    represent, as a whole, an original work of authorship. For the purposes
+#    of this License, Derivative Works shall not include works that remain
+#    separable from, or merely link (or bind by name) to the interfaces of,
+#    the Work and Derivative Works thereof.
+#
+#    "Contribution" shall mean any work of authorship, including
+#    the original version of the Work and any modifications or additions
+#    to that Work or Derivative Works thereof, that is intentionally
+#    submitted to Licensor for inclusion in the Work by the copyright owner
+#    or by an individual or Legal Entity authorized to submit on behalf of
+#    the copyright owner. For the purposes of this definition, "submitted"
+#    means any form of electronic, verbal, or written communication sent
+#    to the Licensor or its representatives, including but not limited to
+#    communication on electronic mailing lists, source code control systems,
+#    and issue tracking systems that are managed by, or on behalf of, the
+#    Licensor for the purpose of discussing and improving the Work, but
+#    excluding communication that is conspicuously marked or otherwise
+#    designated in writing by the copyright owner as "Not a Contribution."
+#
+#    "Contributor" shall mean Licensor and any individual or Legal Entity
+#    on behalf of whom a Contribution has been received by Licensor and
+#    subsequently incorporated within the Work.
+#
+# 2. Grant of Copyright License. Subject to the terms and conditions of
+#    this License, each Contributor hereby grants to You a perpetual,
+#    worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+#    copyright license to reproduce, prepare Derivative Works of,
+#    publicly display, publicly perform, sublicense, and distribute the
+#    Work and such Derivative Works in Source or Object form.
+#
+# 3. Grant of Patent License. Subject to the terms and conditions of
+#    this License, each Contributor hereby grants to You a perpetual,
+#    worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+#    (except as stated in this section) patent license to make, have made,
+#    use, offer to sell, sell, import, and otherwise transfer the Work,
+#    where such license applies only to those patent claims licensable
+#    by such Contributor that are necessarily infringed by their
+#    Contribution(s) alone or by combination of their Contribution(s)
+#    with the Work to which such Contribution(s) was submitted. If You
+#    institute patent litigation against any entity (including a
+#    cross-claim or counterclaim in a lawsuit) alleging that the Work
+#    or a Contribution incorporated within the Work constitutes direct
+#    or contributory patent infringement, then any patent licenses
+#    granted to You under this License for that Work shall terminate
+#    as of the date such litigation is filed.
+#
+# 4. Redistribution. You may reproduce and distribute copies of the
+#    Work or Derivative Works thereof in any medium, with or without
+#    modifications, and in Source or Object form, provided that You
+#    meet the following conditions:
+#
+#    (a) You must give any other recipients of the Work or
+#        Derivative Works a copy of this License; and
+#
+#    (b) You must cause any modified files to carry prominent notices
+#        stating that You changed the files; and
+#
+#    (c) You must retain, in the Source form of any Derivative Works
+#        that You distribute, all copyright, patent, trademark, and
+#        attribution notices from the Source form of the Work,
+#        excluding those notices that do not pertain to any part of
+#        the Derivative Works; and
+#
+#    (d) If the Work includes a "NOTICE" text file as part of its
+#        distribution, then any Derivative Works that You distribute must
+#        include a readable copy of the attribution notices contained
+#        within such NOTICE file, excluding those notices that do not
+#        pertain to any part of the Derivative Works, in at least one
+#        of the following places: within a NOTICE text file distributed
+#        as part of the Derivative Works; within the Source form or
+#        documentation, if provided along with the Derivative Works; or,
+#        within a display generated by the Derivative Works, if and
+#        wherever such third-party notices normally appear. The contents
+#        of the NOTICE file are for informational purposes only and
+#        do not modify the License. You may add Your own attribution
+#        notices within Derivative Works that You distribute, alongside
+#        or as an addendum to the NOTICE text from the Work, provided
+#        that such additional attribution notices cannot be construed
+#        as modifying the License.
+#
+#    You may add Your own copyright statement to Your modifications and
+#    may provide additional or different license terms and conditions
+#    for use, reproduction, or distribution of Your modifications, or
+#    for any such Derivative Works as a whole, provided Your use,
+#    reproduction, and distribution of the Work otherwise complies with
+#    the conditions stated in this License.
+#
+# 5. Submission of Contributions. Unless You explicitly state otherwise,
+#    any Contribution intentionally submitted for inclusion in the Work
+#    by You to the Licensor shall be under the terms and conditions of
+#    this License, without any additional terms or conditions.
+#    Notwithstanding the above, nothing herein shall supersede or modify
+#    the terms of any separate license agreement you may have executed
+#    with Licensor regarding such Contributions.
+#
+# 6. Trademarks. This License does not grant permission to use the trade
+#    names, trademarks, service marks, or product names of the Licensor,
+#    except as required for reasonable and customary use in describing the
+#    origin of the Work and reproducing the content of the NOTICE file.
+#
+# 7. Disclaimer of Warranty. Unless required by applicable law or
+#    agreed to in writing, Licensor provides the Work (and each
+#    Contributor provides its Contributions) on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+#    implied, including, without limitation, any warranties or conditions
+#    of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
+#    PARTICULAR PURPOSE. You are solely responsible for determining the
+#    appropriateness of using or redistributing the Work and assume any
+#    risks associated with Your exercise of permissions under this License.
+#
+# 8. Limitation of Liability. In no event and under no legal theory,
+#    whether in tort (including negligence), contract, or otherwise,
+#    unless required by applicable law (such as deliberate and grossly
+#    negligent acts) or agreed to in writing, shall any Contributor be
+#    liable to You for damages, including any direct, indirect, special,
+#    incidental, or consequential damages of any character arising as a
+#    result of this License or out of the use or inability to use the
+#    Work (including but not limited to damages for loss of goodwill,
+#    work stoppage, computer failure or malfunction, or any and all
+#    other commercial damages or losses), even if such Contributor
+#    has been advised of the possibility of such damages.
+#
+# 9. Accepting Warranty or Additional Liability. While redistributing
+#    the Work or Derivative Works thereof, You may choose to offer,
+#    and charge a fee for, acceptance of support, warranty, indemnity,
+#    or other liability obligations and/or rights consistent with this
+#    License. However, in accepting such obligations, You may act only
+#    on Your own behalf and on Your sole responsibility, not on behalf
+#    of any other Contributor, and only if You agree to indemnify,
+#    defend, and hold each Contributor harmless for any liability
+#    incurred by, or claims asserted against, such Contributor by reason
+#    of your accepting any such warranty or additional liability.
+#
+# END OF TERMS AND CONDITIONS
+#
+# APPENDIX: How to apply the Apache License to your work.
+#
+#    To apply the Apache License to your work, attach the following
+#    boilerplate notice, with the fields enclosed by brackets "[]"
+#    replaced with your own identifying information. (Don't include
+#    the brackets!)  The text should be enclosed in the appropriate
+#    comment syntax for the file format. We also recommend that a
+#    file or class name and description of purpose be included on the
+#    same "printed page" as the copyright notice for easier
+#    identification within third-party archives.
+#
+# Copyright 2020 Xuebin Qin et al.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+The code for our newly accepted paper in Pattern
+Recognition 2020: "U^2-Net: Going Deeper with Nested
+U-Structure for Salient Object Detection."
+
+FROM: https://github.com/xuebinqin/U-2-Net/blob/master/model/u2net_refactor.py
+
+MODIFICATIONS:
+- cleaned up for use the mtg_ml
+"""
+
+import torch
+import torch.nn as nn
+import math
+
+
+# ========================================================================= #
+# Helper                                                                    #
+# ========================================================================= #
+
+
+def _upsample_like(x, size):
+    return nn.Upsample(size=size, mode='bilinear', align_corners=False)(x)
+
+
+def _size_map(x, height):
+    # {height: size} for Upsample
+    size = list(x.shape[-2:])
+    sizes = {}
+    for h in range(1, height):
+        sizes[h] = size
+        size = [math.ceil(w / 2) for w in size]
+    return sizes
+
+
+# ========================================================================= #
+# Components                                                                #
+# ========================================================================= #
+
+
+class REBNCONV(nn.Module):
+    def __init__(self, in_ch=3, out_ch=3, dilate=1):
+        super(REBNCONV, self).__init__()
+
+        self.conv_s1 = nn.Conv2d(in_ch, out_ch, 3, padding=1 * dilate, dilation=1 * dilate)
+        self.bn_s1 = nn.BatchNorm2d(out_ch)
+        self.relu_s1 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        return self.relu_s1(self.bn_s1(self.conv_s1(x)))
+
+
+class RSU(nn.Module):
+    def __init__(self, name, height, in_ch, mid_ch, out_ch, dilated=False):
+        super(RSU, self).__init__()
+        self.name = name
+        self.height = height
+        self.dilated = dilated
+        self._make_layers(height, in_ch, mid_ch, out_ch, dilated)
+
+    def forward(self, x):
+        sizes = _size_map(x, self.height)
+        x = self.rebnconvin(x)
+
+        # U-Net like symmetric encoder-decoder structure
+        def unet(x, height=1):
+            if height < self.height:
+                x1 = getattr(self, f'rebnconv{height}')(x)
+                if not self.dilated and height < self.height - 1:
+                    x2 = unet(getattr(self, 'downsample')(x1), height + 1)
+                else:
+                    x2 = unet(x1, height + 1)
+
+                x = getattr(self, f'rebnconv{height}d')(torch.cat((x2, x1), 1))
+                return _upsample_like(x, sizes[height - 1]) if not self.dilated and height > 1 else x
+            else:
+                return getattr(self, f'rebnconv{height}')(x)
+
+        return x + unet(x)
+
+    def _make_layers(self, height, in_ch, mid_ch, out_ch, dilated=False):
+        self.add_module('rebnconvin', REBNCONV(in_ch, out_ch))
+        self.add_module('downsample', nn.MaxPool2d(2, stride=2, ceil_mode=True))
+
+        self.add_module(f'rebnconv1', REBNCONV(out_ch, mid_ch))
+        self.add_module(f'rebnconv1d', REBNCONV(mid_ch * 2, out_ch))
+
+        for i in range(2, height):
+            dilate = 1 if not dilated else 2 ** (i - 1)
+            self.add_module(f'rebnconv{i}', REBNCONV(mid_ch, mid_ch, dilate=dilate))
+            self.add_module(f'rebnconv{i}d', REBNCONV(mid_ch * 2, mid_ch, dilate=dilate))
+
+        dilate = 2 if not dilated else 2 ** (height - 1)
+        self.add_module(f'rebnconv{height}', REBNCONV(mid_ch, mid_ch, dilate=dilate))
+
+
+# ========================================================================= #
+# Models                                                                    #
+# ========================================================================= #
+
+
+class U2NET(nn.Module):
+
+    def __init__(
+        self,
+        cfgs: dict,
+        out_ch: int,
+    ):
+        super(U2NET, self).__init__()
+        self.out_ch = out_ch
+        self._make_layers(cfgs)
+
+    def forward(self, x):
+        sizes = _size_map(x, self.height)
+        maps = []  # storage for maps
+
+        # side saliency map
+        def unet(x, height=1):
+            if height < 6:
+                x1 = getattr(self, f'stage{height}')(x)
+                x2 = unet(getattr(self, 'downsample')(x1), height + 1)
+                x = getattr(self, f'stage{height}d')(torch.cat((x2, x1), 1))
+                side(x, height)
+                return _upsample_like(x, sizes[height - 1]) if height > 1 else x
+            else:
+                x = getattr(self, f'stage{height}')(x)
+                side(x, height)
+                return _upsample_like(x, sizes[height - 1])
+
+        def side(x, h):
+            # side output saliency map (before sigmoid)
+            x = getattr(self, f'side{h}')(x)
+            x = _upsample_like(x, sizes[1])
+            maps.append(x)
+
+        def fuse():
+            # fuse saliency probability maps
+            maps.reverse()
+            x = torch.cat(maps, 1)
+            x = getattr(self, 'outconv')(x)
+            maps.insert(0, x)
+            return [torch.sigmoid(x) for x in maps]
+
+        unet(x)
+        maps = fuse()
+        return maps
+
+    def _make_layers(self, cfgs):
+        self.height = int((len(cfgs) + 1) / 2)
+        self.add_module('downsample', nn.MaxPool2d(2, stride=2, ceil_mode=True))
+        for k, v in cfgs.items():
+            # build rsu block
+            self.add_module(k, RSU(v[0], *v[1]))
+            if v[2] > 0:
+                # build side layer
+                self.add_module(f'side{v[0][-1]}', nn.Conv2d(v[2], self.out_ch, 3, padding=1))
+        # build fuse layer
+        self.add_module('outconv', nn.Conv2d(int(self.height * self.out_ch), self.out_ch, 1))
+
+
+def U2NET_full(out_ch: int = 1):
+    full = {
+        # cfgs for building RSUs and sides
+        # {stage : [name, (height(L), in_ch, mid_ch, out_ch, dilated), side]}
+        'stage1': ['En_1', (7, 3, 32, 64), -1],
+        'stage2': ['En_2', (6, 64, 32, 128), -1],
+        'stage3': ['En_3', (5, 128, 64, 256), -1],
+        'stage4': ['En_4', (4, 256, 128, 512), -1],
+        'stage5': ['En_5', (4, 512, 256, 512, True), -1],
+        'stage6': ['En_6', (4, 512, 256, 512, True), 512],
+        'stage5d': ['De_5', (4, 1024, 256, 512, True), 512],
+        'stage4d': ['De_4', (4, 1024, 128, 256), 256],
+        'stage3d': ['De_3', (5, 512, 64, 128), 128],
+        'stage2d': ['De_2', (6, 256, 32, 64), 64],
+        'stage1d': ['De_1', (7, 128, 16, 64), 64],
+    }
+    return U2NET(cfgs=full, out_ch=out_ch)
+
+
+
+def U2NET_lite(out_ch: int = 1):
+    lite = {
+        # cfgs for building RSUs and sides
+        # {stage : [name, (height(L), in_ch, mid_ch, out_ch, dilated), side]}
+        'stage1': ['En_1', (7, 3, 16, 64), -1],
+        'stage2': ['En_2', (6, 64, 16, 64), -1],
+        'stage3': ['En_3', (5, 64, 16, 64), -1],
+        'stage4': ['En_4', (4, 64, 16, 64), -1],
+        'stage5': ['En_5', (4, 64, 16, 64, True), -1],
+        'stage6': ['En_6', (4, 64, 16, 64, True), 64],
+        'stage5d': ['De_5', (4, 128, 16, 64, True), 64],
+        'stage4d': ['De_4', (4, 128, 16, 64), 64],
+        'stage3d': ['De_3', (5, 128, 16, 64), 64],
+        'stage2d': ['De_2', (6, 128, 16, 64), 64],
+        'stage1d': ['De_1', (7, 128, 16, 64), 64],
+    }
+    return U2NET(cfgs=lite, out_ch=out_ch)
