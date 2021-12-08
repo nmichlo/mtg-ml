11326f11492d8a6fe688ba8c0fcfa45c9d9cf669
Nathan Michlo <NathanJMichlo@gmail.com>
2021-12-08 16:42:58 +0200

    working docker

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker-compose run --rm mtgml39 "$@"
+docker build . -t mtgml39
+
+docker run \
+    --rm -it --gpus=all --shm-size=1G \
+    -v ~/workspace/playground/mtg-ml/mtg_ml:/workspace/mtg_ml \
+    -v ~/workspace/playground/mtg-ml/experiment:/workspace/experiment \
+    -v ~/workspace/playground/mtg-ml/out:/workspace/out \
+    -v ~/workspace/playground/mtg-dataset/data:/data \
+    mtgml39 \
+    "$@"
